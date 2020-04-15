d006a799b3733e4b3ebe452ddfaffe33f9c48fdb
eyasuh <eyha12@gmail.com>
2020-04-15 09:28:16 -0400

    first commit

---
+++ src/DataTypeVariables.java
@@ -0,0 +1,14 @@
+public class DataTypeVariables {
+        public static void main(String [] args) {
+            // declare and intialize
+            int testScore = 50;
+            double price = 24.99;
+            String username = "Bobby Johnson";
+            // print output
+            System.out.println("Test Score initialized is: " + testScore);
+            System.out.println("Price tag: " + price);
+            System.out.println("Username: " + username);
+
+        }
+    }
+
